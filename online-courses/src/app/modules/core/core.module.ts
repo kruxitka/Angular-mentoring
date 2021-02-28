8994d487ae9084f0363b0abd086c58748524c270
kruxitka <kruxitka@gmail.com>
2021-02-28 09:22:47 +0200

    added angular material

--- online-courses/src/app/modules/core/core.module.ts
+++ online-courses/src/app/modules/core/core.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoreRoutingModule } from './core-routing.module';
 import * as components from './components';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 const COMPONENTS = [
   components.LogoComponent,
@@ -13,7 +15,7 @@ const COMPONENTS = [
 
 @NgModule({
   declarations: [...COMPONENTS],
-  imports: [CommonModule, CoreRoutingModule],
+  imports: [CommonModule, CoreRoutingModule, MatButtonModule, MatIconModule],
   exports: [components.LayoutComponent],
 })
 export class CoreModule { }
