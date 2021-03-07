b0ddab4b93231efdc7e7e0bc52af12f19539f59d
kruxitka <kruxitka@gmail.com>
2021-03-07 23:38:34 +0200

    added test for course-item

--- online-courses/src/app/modules/courses/components/course-item/course-item.component.spec.ts
+++ online-courses/src/app/modules/courses/components/course-item/course-item.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { Course } from '../../interfaces/course.interface';
 
 import { CourseItemComponent } from './course-item.component';
 
@@ -21,5 +23,17 @@ describe('CourseItemComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('course id is emitted when click Delete button', () => {
+    const comp = new CourseItemComponent();
+    const course = {
+      id: '1',
+      title: 'Course title 1',
+    };
+    comp.course = course;
+
+    comp.deleteCourse.subscribe((itemToDelete) => expect(itemToDelete).toBe(course.id));
+    comp.delete();
   });
 });
