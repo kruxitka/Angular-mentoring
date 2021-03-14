bee5d16f2832facc8d726a08d2e87e5fc063b196
kruxitka <kruxitka@gmail.com>
2021-03-14 23:14:13 +0200

    added pipes

---
+++ online-courses/src/app/modules/courses/directives/highlight-newest.directive.spec.ts
@@ -0,0 +1,8 @@
+import { HighlightNewestDirective } from './highlight-newest.directive';
+
+describe('HighlightNewestDirective', () => {
+  it('should create an instance', () => {
+    const directive = new HighlightNewestDirective();
+    expect(directive).toBeTruthy();
+  });
+});
