7ed77508089145c3000d7926503b31a221a73f39
kruxitka <kruxitka@gmail.com>
2021-03-21 23:32:35 +0200

    added courses service

--- online-courses/src/app/modules/courses/components/courses-list/courses-list.component.ts
+++ online-courses/src/app/modules/courses/components/courses-list/courses-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Course } from '../../interfaces/course.interface';
+import { CoursesService } from '../../services/courses.service';
 
 @Component({
   selector: 'app-courses-list',
@@ -7,43 +9,16 @@ import { Course } from '../../interfaces/course.interface';
   styleUrls: ['./courses-list.component.scss']
 })
 export class CoursesListComponent implements OnInit {
-  public coursesList: Course[] = [
-    {
-      id: '1',
-      title: 'Course title 1',
-      duration: '1h 15min',
-      creationDate: 'August 19, 2020 23:15:30',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-    {
-      id: '2',
-      title: 'Course title 2',
-      duration: '1h 15min',
-      creationDate: 'May 01, 2019 23:15:30',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-    {
-      id: '3',
-      title: 'Course title 3',
-      duration: '1h 15min',
-      creationDate: 'September 24, 2020 12:15:30',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    },
-    {
-      id: '4',
-      title: 'Course title 4',
-      duration: '1h 15min',
-      creationDate: 'June 05, 2020 23:15:30',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-    }
-  ];
+  public coursesList$: Observable<Course[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private coursesService: CoursesService) {
+    this.coursesList$ = this.coursesService.getCoursesList();
   }
 
+  ngOnInit(): void {}
+
   public onDeleteCourse(courseId: string): void {
+    this.coursesList$ = this.coursesService.deleteCourse(courseId);
     console.log(courseId);
   }
 
---
+++ online-courses/src/app/modules/courses/services/courses.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Course } from '../interfaces/course.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  public coursesList: Course[] = [
+    {
+      id: '1',
+      title: 'Course title 1',
+      duration: '1h 15min',
+      creationDate: 'August 19, 2020 23:15:30',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+      id: '2',
+      title: 'Course title 2',
+      duration: '1h 15min',
+      creationDate: 'May 01, 2019 23:15:30',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+      id: '3',
+      title: 'Course title 3',
+      duration: '1h 15min',
+      creationDate: 'September 24, 2020 12:15:30',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    },
+    {
+      id: '4',
+      title: 'Course title 4',
+      duration: '1h 15min',
+      creationDate: 'June 05, 2020 23:15:30',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    }
+  ];
+
+  constructor() { }
+
+  public getCoursesList(): Observable<Course[]> {
+    return of(this.coursesList);
+  }
+
+  public createCourse(course: Course): void {
+    this.coursesList.push(course);
+  }
+
+  public getCourseInfoById(courseId: string): Observable<Course> {
+    return of(...this.coursesList.filter(el => el.id === courseId));
+  }
+
+  public updateCourse(course: Course) {
+
+  }
+
+  public deleteCourse(courseId: string) {
+    return of(this.coursesList.filter(el => el.id !== courseId));
+  }
+}
