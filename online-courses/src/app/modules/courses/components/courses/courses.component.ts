a1d068eeb59f661e8cbd6fb5373f50065a8cde33
kruxitka <kruxitka@gmail.com>
2021-05-01 11:22:02 +0300

    fixed courses list

--- online-courses/src/app/modules/courses/components/courses/courses.component.ts
+++ online-courses/src/app/modules/courses/components/courses/courses.component.ts
@@ -1,6 +1,8 @@
 import { Course } from './../../interfaces/course.interface';
 import { Component, OnInit } from '@angular/core';
 import { FilterPipe } from 'src/app/modules/shared/modules/search/pipes/filter.pipe';
+import { Observable } from 'rxjs';
+import { CoursesService } from '../../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -10,51 +12,10 @@ import { FilterPipe } from 'src/app/modules/shared/modules/search/pipes/filter.p
 export class CoursesComponent implements OnInit {
   public coursesList!: Course[];
 
-  constructor(private filterPipe: FilterPipe) { }
+  constructor(private filterPipe: FilterPipe, private coursesService: CoursesService) {}
 
   ngOnInit(): void {
-     this.coursesList = [
-      {
-        id: '1',
-        title: 'Course title 1',
-        duration: 90,
-        creationDate: '2020-08-19T20:15:30.000Z',
-        topRated: false,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      },
-      {
-        id: '2',
-        title: 'Course title 2',
-        duration: 75,
-        creationDate: '2019-07-01T20:15:30.000Z',
-        topRated: true,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      },
-      {
-        id: '3',
-        title: 'Course title 3',
-        duration: 120,
-        creationDate: '2021-05-01T20:15:30.000Z',
-        topRated: false,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      },
-      {
-        id: '4',
-        title: 'Course title 4',
-        duration: 60,
-        creationDate: '2021-04-10T20:15:30.000Z',
-        topRated: false,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      },
-      {
-        id: '5',
-        title: 'Course filter test',
-        duration: 90,
-        creationDate: '2020-08-19T20:15:30.000Z',
-        topRated: false,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      },
-    ];
+    this.coursesService.getCoursesList().subscribe(courses => this.coursesList = courses);
   }
 
   public onSearchCourse(searchText: string): any {
